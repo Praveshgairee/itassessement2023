0569ba229655e8edd387ab7d39467dfa72214915
PravesH <gairepravesh@gmail.com>
2023-04-24 23:13:42 +0545

    CSS & JS & Remaining all Content

---
+++ CSS & JS & IMG/contact.js
@@ -0,0 +1,12 @@
+var blast = document.querySelector(".blast");
+var quit = document.querySelector(".icon-close");
+
+blast.addEventListener("click", ()=>{
+    form.classList.add("active");
+});
+
+
+
+
+
+
